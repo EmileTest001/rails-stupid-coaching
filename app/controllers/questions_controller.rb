749eb7ec0f2d0cea8cd0f8d7f13f6009db6d51e7
Emile Nguyen <nguyen_emile@yahoo.fr>
2020-11-09 12:43:37 +0100

    Q&A and interactivity OK

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,5 +6,20 @@ class QuestionsController < ApplicationController
   def answer
     # 1. créer cette méthode
     # 2. puis créer dans config/routes.rb la méthode get
+    @my_question = params[:question]
+    case @my_question
+    when "hello"
+      @coach_answer = "Great!"
+    when "what time is it?"
+      @coach_answer = Time.now
+    when @my_question.end_with?("?") == true
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      if @my_question.end_with?("?")
+        @coach_answer = "Silly question, get dressed and go to work!"
+      else
+        @coach_answer = "I don't care, get dressed and go to work!"
+      end
+    end
   end
 end
