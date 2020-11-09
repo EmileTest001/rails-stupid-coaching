9161f9f1d1888a5b1fcab28188195eb828180943
Emile Nguyen <nguyen_emile@yahoo.fr>
2020-11-09 11:44:13 +0100

    Both pages ASK & ANSWER OK. Remaining the Q&A part

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,10 @@
+class QuestionsController < ApplicationController
+  def ask
+    # 1. créer cette méthode
+    # 2. puis créer dans config/routes.rb la méthode get
+  end
+  def answer
+    # 1. créer cette méthode
+    # 2. puis créer dans config/routes.rb la méthode get
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Partie ask
+  get "ask", to: "questions#ask"
+  # 3. ajouter cette phrase
+  # 4.créer la vue associée app/views/questions/ask.html.erb
+
+  # Partie answer
+  get "answer", to: "questions#answer"
+  # 3. ajouter cette phrase
+  # 4.créer la vue associée app/views/questions/answer.html.erb
 end
